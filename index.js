7bf5d813d2c14c47f0f804097ed439f58a7b8241
john <john.d.leonetti@gmail.com>
2021-06-07 09:52:55 -0700

    commit tree f0c018ad151a450585de02b12170b1d35684ee80

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    let leftNumbers = dodger.style.left.replace("px", " ");
+    let left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("key down", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (e.key === "ArrowRight") {
+        moveDodgeRight();
+    }
+});
+
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.right.replace("px", " ");
+    let right = parseInt(rightNumbers, 10);
+    if (right > 180) {
+        dodger.style.right = `${right + 1}px`;
+    }
+}
+dodger.style.backgroundColor = "#FF69b4";
+//console.log(dodger.style.left);
+//console.log(dodger.style.bottom);
+ dodger.style.bottom = "0px"
+
+// document.addEventListener("keydown", function(e) {
+//     console.log(e.key === "ArrowLeft")
+//     {
+//         let leftNumbers = dodger.style.left.replace("px", "");
+//         let left = parseInt(leftNumbers, 10);
+//         dodger.style.left = `${left - 1}px`;
+//     }
+// });
